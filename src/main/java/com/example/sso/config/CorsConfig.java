bb6bab314aa270dd166aa47f6021aae25be183e8
resline <kamilkonera@gmail.com>
2024-07-16 13:26:45 +0200

    Update CorsConfig.java

--- src/main/java/com/example/sso/config/CorsConfig.java
+++ src/main/java/com/example/sso/config/CorsConfig.java
@@ -17,7 +17,10 @@ public class CorsConfig {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		CorsConfiguration config = new CorsConfiguration();
 		config.setAllowCredentials(true);
-		config.addAllowedOrigin("http://localhost:4200, http://127.0.0.1:4200, https://127.0.0.1:8443"); // Angular app's URL
+		config.addAllowedOrigin("http://localhost:4200");
+		config.addAllowedOrigin("http://localhost:8443");
+		config.addAllowedOrigin("http://127.0.0.1:4200");
+		config.addAllowedOrigin("https://127.0.0.1:8443");
 		config.addAllowedHeader("*");
 		config.addAllowedMethod("*");
 		config.addExposedHeader("Set-Cookie");
